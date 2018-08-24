10c3d8581a05b5f73951d41072b5f9dc76715c52
Yoann Grange <yoann.grange@gmail.com>
2018-08-24 16:15:57 +0200

    switch indices in algolia

--- search.php
+++ search.php
@@ -3,27 +3,30 @@ require_once('vendor/algolia/algoliasearch.php');
 
 $client = new \AlgoliaSearch\Client('BG3AX4OCRB', '0a5e0edb3588b17ea4255c35f077e00d');
 
-$index = $client->initIndex('catalogues');
+$index = $client->initIndex('catalogues_v2');
 
 $index->setSettings(array(
   "searchableAttributes" => [
     "serie",
-    "model"
+    "range"
   ],
   "customRanking" => [
     "asc(serie)",
-    "asc(brand)"
+    "asc(brand)",
+    "asc(range)"
   ]
 ));
 
 
-$manuals = $client->initIndex('manuals');
+$manuals = $client->initIndex('manuals_v2');
 
 $manuals->setSettings(array(
   "searchableAttributes" => [
     "model",
+    "range",
     "filename",
     "year",
+    "brand",
     "language"
   ],
   "customRanking" => [
@@ -35,20 +38,25 @@ $manuals->setSettings(array(
 ));
 
 
-$parts = $client->initIndex('parts');
+$parts = $client->initIndex('parts_v2');
 
 $parts->setSettings(array(
   "searchableAttributes" => [
-    "ref",
-    "label_en",
-    "label_fr",
-    "label_de"
+    "reference",
+    "fr",
+    "en",
+    "de",
+    "it",
+    "pl",
+    "ru",
+    "uk",
+    "brand"
   ],
   "customRanking" => [
-    "asc(ref)",
-    "asc(label_en)",
-    "asc(label_fr)",
-    "asc(label_de)",
+    "asc(reference)",
+    "asc(fr)",
+    "asc(en)",
+    "asc(de)",
     "asc(price_eur)",
     "asc(weight)",
     "asc(brand)"
