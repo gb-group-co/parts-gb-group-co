d8f39267f1e2c421969e9960d55a71f7b6466bee
Julien Richard <44161762+JulienYvesRICHARD@users.noreply.github.com>
2019-01-03 16:21:10 +0100

    adding the script for the session storage for the cookies banner

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+const cookiesBanner = () => {
+    let cookies = sessionStorage.getItem('user')
+    if (cookies != 'cookiesAccepted') {
+        document.getElementById("bandeau-cookies").style.display= "inline"
+    }
+    sessionStorage.setItem('user', 'cookiesAccepted');
+}
+
+cookiesBanner();
+
+
 const infoRemove = () => {
-    document.getElementById("bandeau-cookies").style.display="none"
+    sessionStorage.setItem('user', 'cookiesAccepted');
+    document.getElementById("bandeau-cookies").style.display = "none"
 }
